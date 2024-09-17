b5622b1257d933bd859c2a1700dda4bf0e6f364e
Ryan Lei <135535159+RyanL3i@users.noreply.github.com>
2024-09-17 18:44:46 -0500

    changed flask

--- backend/flask_server.py
+++ backend/flask_server.py
@@ -16,5 +16,12 @@ def get_players():                                 # function to be triggered to
     data = scrape_all_players.scrape()              # data is returned from scrape function
     return jsonify(data)                            # turn the data into json format!
 
+
+@app.route('/api/run-scraper', methods=['POST'])        # when accessing at the url, POSTs a request to run scraper!
+def run_scraper():
+    # Run the scraper
+    scrape_all_players.scrape()
+    return jsonify({"status": "scraper run successfully"}), 200
+
 if __name__ == '__main__':                      # runs only if the "main" to run is "name" (defined above)
     app.run(debug=True, port=5002)
--- backend/run_scraper.py
+++
@@ -1,7 +0,0 @@
-import scrape_all_players
-
-def run():
-    scrape_all_players.scrape()
-
-if __name__ == '__main__':
-    run()
